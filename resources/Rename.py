a275fb55f3da528c1c8abc4b725b3acd8656bda8
logandelaar1 <127620732+logandelaar1@users.noreply.github.com>
2023-07-07 12:06:02 -0400

    Add files via upload

---
+++ resources/Rename.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+
+# Source folder containing the original .jpg photos
+source_folder = "/Users/logandelaar/Desktop/YOLO-moos/IMAGE_Processing/Filtered"
+
+# Destination folder to save the renamed .jpg photos
+destination_folder = "/Users/logandelaar/Desktop/YOLO-moos/IMAGE_Processing/Renamed"
+
+# Create the destination folder if it doesn't exist
+os.makedirs(destination_folder, exist_ok=True)
+
+# Get a list of all .jpg files in the source folder
+jpg_files = [file for file in os.listdir(source_folder) if file.endswith(".jpg")]
+
+# Sort the files alphabetically
+jpg_files.sort()
+
+#Get a name for begining of file
+name_of_file = input("What should it be called? ")
+#Get persons name
+
+persons_name = input("What is your last name? ")
+# Counter to keep track of the numerical order
+counter = 1
+
+# Iterate through the .jpg files
+for file in jpg_files:
+    # Create the new filename with the counter value
+    new_filename = f"{name_of_file}_{persons_name}_{counter}.jpg"
+
+    # Build the full paths of the source and destination files
+    source_path = os.path.join(source_folder, file)
+    destination_path = os.path.join(destination_folder, new_filename)
+
+    # Rename and move the file to the destination folder
+    shutil.move(source_path, destination_path)
+
+    # Increment the counter
+    counter += 1
+
+print("Renaming and moving complete.")
---
+++ resources/filter.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+import shutil
+
+# Specify the source directory and target directory
+src_dir = '/Users/logandelaar/Desktop/YOLO-moos/IMAGE_Processing/Rotated'
+target_dir = '/Users/logandelaar/Desktop/YOLO-moos/IMAGE_Processing/Filtered'
+
+# Make sure the target directory exists 
+os.makedirs(target_dir, exist_ok=True)
+
+# Walk through all files in the source directory
+for filename in os.listdir(src_dir):
+    if filename.endswith('.jpg'):
+        # Create the full file path by joining the source directory and the filename
+        filepath = os.path.join(src_dir, filename)
+
+        # Open the image file with OpenCV
+        img = cv2.imread(filepath)
+
+        # Show the image in a window
+        cv2.imshow('Image', img)
+
+        # Wait for keypress
+        key = cv2.waitKey(0) & 0xFF
+
+        # If 'd' is pressed, delete the file
+        if key == ord('d'):
+            os.remove(filepath)
+
+        # If space bar is pressed, move the image to the target directory
+        elif key == 32:  # ASCII for space is 32
+            # Create the target file path
+            target_filepath = os.path.join(target_dir, filename)
+
+            # Move the file to the target directory
+            shutil.move(filepath, target_filepath)
+
+        # Close the window
+        cv2.destroyAllWindows()
+
+print("All .jpg files have been processed.")
